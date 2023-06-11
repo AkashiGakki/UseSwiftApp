26e9227f43a2a957380c7e35ef957b1a23316f53
Akashi <akashi_sai@163.com>
2023-06-11 16:46:08 +0800

    Initial Commit

---
+++ UseSwiftApp/UseSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseSwiftAppApp.swift
+//  UseSwiftApp
+//
+//  Created by Akashi on 2023/6/11.
+//
+
+import SwiftUI
+
+@main
+struct UseSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
